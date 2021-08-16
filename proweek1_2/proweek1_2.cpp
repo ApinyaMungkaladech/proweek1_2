6678f052413307ead7d7729f38df8e3d98d8fa4d
APINYA MUNGKALADECH <64010971@KMITL.AC.TH>
2021-08-16 21:08:44 +0700

    perfect update

--- proweek1_2/proweek1_2.cpp
+++ proweek1_2/proweek1_2.cpp
@@ -8,19 +8,21 @@ int main() {
 	printf("Enter number : ");
 	scanf_s("%d", &n3);
 
-	if (n1 > n2) {
+	if (n1 >= n2) {
 		if (n1 > n3) {
 			printf("%d", n1);
 		}
 	}
 	if (n2 > n1) {
-		if (n2 > n3) {
+		if (n2 >= n3) {
 			printf("%d", n2);
 		}
 	}
-	if (n3 > n1) {
+	if (n3 >= n1) {
 		if (n3 > n2) {
 			printf("%d", n3);
 		}
 	}
+	if (n1 == n2 && n1 == n3) 
+		printf("%d", n1);
 }
